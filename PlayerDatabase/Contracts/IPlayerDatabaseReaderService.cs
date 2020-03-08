3842f5f0c744836dde5f720290666c133ae199f4
Matthew P. Del Buono <mpdelbuono@gmail.com>
2020-03-07 17:58:23 -0800

    Add reader/writer service contracts for the PlayerDatabase service

--- PlayerDatabase/Contracts/IPlayerDatabaseReaderService.cs
+++ PlayerDatabase/Contracts/IPlayerDatabaseReaderService.cs
@@ -1,11 +1,59 @@
 ﻿namespace Abacus.BackEnds.PlayerDatabase.Contracts
 {
     using Microsoft.ServiceFabric.Services.Remoting;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// Interface to accessing a player database API as a read-only API.
     /// </summary>
     public interface IPlayerDatabaseReaderService : IService
     {
+        /// <summary>
+        /// Gets the data associated with the character. If no data is stored,
+        /// then <see langword="null"/> is returned. Only the insecure data is returned, i.e.,
+        /// the data written by <see cref="IPlayerDatabaseWriterService.WritePlayerDataAsync{T}(int, T)"/>.
+        /// </summary>
+        /// <param name="characterId">The EVE ID of the character.</param>
+        /// <returns>The data stored on the character, or <see langword="null"/> if no
+        /// data is stored.</returns>
+        /// <remarks>
+        /// This data is only the data which is stored generically, without
+        /// reference to which account owns the character. To access the secure
+        /// (account-bound) information, use <see cref="GetSecureCharacterDataAsync(int, string)"/>.
+        /// </remarks>
+        /// <exception cref="System.Fabric.FabricNotReadableException">The database
+        /// is having trouble reading the data stored against the player.</exception>
+        /// <exception cref="System.TimeoutException">A timeout occurred while trying
+        /// to read the database. This is most frequently caused by either lock
+        /// contention or database quorum loss.</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">
+        /// The data that is stored is incompatible with type <typeparamref name="T"/>.</exception>
+        Task<T?> GetPlayerDataAsync<T>(int characterId)
+            where T : class;
+
+        /// <summary>
+        /// Gets the data associated with the player, including any secure data associated
+        /// with the player, stored by <see cref="IPlayerDatabaseWriterService.WriteSecurePlayerDataAsync{T}(int, string, T)"/>.
+        /// If no data is stored, then <see langword="null"/> is returned.
+        /// Secure data for other accounts that owned the character is not returned.
+        /// </summary>
+        /// <param name="characterId">The EVE ID of the character.</param>
+        /// <param name="secureHash">The ESI secure hash of the player.</param>
+        /// <returns>The data stored on the character, or <see langword="null"/> if no
+        /// data is stored.</returns>
+        /// <remarks>This data is a superset of that data that would be returned
+        /// from <see cref="GetPlayerDataAsync(int)"/>. The <paramref name="secureHash"/>
+        /// is guaranteed unique for the combination of a character and the player's
+        /// login account. This means if the character is transferred to another account,
+        /// the hash will change.</remarks>
+        /// <exception cref="System.Fabric.FabricNotReadableException">The database
+        /// is having trouble reading the data stored against the player.</exception>
+        /// <exception cref="System.TimeoutException">A timeout occurred while trying
+        /// to read the database. This is most frequently caused by either lock
+        /// contention or database quorum loss.</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">
+        /// The data that is stored is incompatible with type <typeparamref name="T"/>.</exception>
+        Task<T?> GetSecureCharacterDataAsync<T>(int characterId, string secureHash)
+            where T : class;
     }
 }
--- PlayerDatabase/Contracts/IPlayerDatabaseWriterService.cs
+++ PlayerDatabase/Contracts/IPlayerDatabaseWriterService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Abacus.BackEnds.PlayerDatabase.Contracts
 {
+    using System.Threading.Tasks;
     using Microsoft.ServiceFabric.Services.Remoting;
 
     /// <summary>
@@ -7,5 +8,46 @@
     /// </summary>
     public interface IPlayerDatabaseWriterService : IService
     {
+        /// <summary>
+        /// Writes data for the character into the database, replacing the data that was previously present.
+        /// This data is stored into the insecure table which does not depend upon the character's account hash.
+        /// To depend on the account hash (to clear the data after account transfers), see
+        /// <see cref="WriteSecurePlayerDataAsync{T}(int, string, T)"/>.
+        /// </summary>
+        /// <param name="characterId">The ESI character ID of the character for which the data is stored.</param>
+        /// <param name="data">The data to store.</param>
+        /// <typeparam name="T">The type of data to store.</typeparam>
+        /// <returns>A <see cref="Task"/> upon which the operation can be awaited.</returns>
+        /// <exception cref="System.Runtime.Serialization.InvalidDataContractException">
+        /// The type <typeparamref name="T"/> is not serializable by
+        /// <see cref="System.Runtime.Serialization.DataContractSerializer"/>.
+        /// </exception>
+        /// <exception cref="System.TimeoutException">
+        /// Writing to the database timed out. The most common causes for this are that the primary is
+        /// unhealthy or the partition is in quorum loss.</exception>
+        Task WritePlayerDataAsync<T>(int characterId, T data)
+            where T : class;
+
+        /// <summary>
+        /// Writes data for the character into the database, replacing
+        /// data which was previously present. This data is stored into the secure table
+        /// and only accessible via <see cref="IPlayerDatabaseReaderService.GetSecureCharacterDataAsync(int, string)"/>.
+        /// The secure table ensures that the appropriate hash is provided, so that if the character
+        /// changes accounts, the data is cleared.
+        /// </summary>
+        /// <param name="characterId">The ESI character ID of the character for which the data is stored.</param>
+        /// <param name="secureHash">The secure hash of the ESI character ID and account.</param>
+        /// <param name="data">The data to store.</param>
+        /// <typeparam name="T">The type of data to store.</typeparam>
+        /// <returns>A <see cref="Task"/> upon which the operation can be awaited.</returns>
+        /// <exception cref="System.Runtime.Serialization.InvalidDataContractException">
+        /// The type <typeparamref name="T"/> is not serializable by
+        /// <see cref="System.Runtime.Serialization.DataContractSerializer"/>.
+        /// </exception>
+        /// <exception cref="System.TimeoutException">
+        /// Writing to the database timed out. The most common causes for this are that the primary is
+        /// unhealthy or the partition is in quorum loss.</exception>
+        Task WriteSecurePlayerDataAsync<T>(int characterId, string secureHash, T data)
+            where T : class;
     }
 }
