7be3abfc6571e364e1b49ea85420249715d5df08
Matthew P. Del Buono <mpdelbuono@gmail.com>
2020-03-09 20:50:54 -0700

    Add auto-generating version numbers based on the latest version tag

---
+++ FleetPresenceTracker/Scripts/Update-ApplicationVersionNumber.ps1
@@ -0,0 +1,4 @@
+﻿# Updates the application manifest to include the latest version number as specified by
+# the latest git tag and commit hash.
+$version = git describe --tags
+(Get-Content -path "..\ApplicationPackageRoot\ApplicationManifest.xml.template") -replace '{VERSION}',$version | Out-File -Encoding ASCII "..\ApplicationPackageRoot\ApplicationManifest.xml"
---
+++ FleetPresenceTracker/Scripts/UpdateApplicationVersionNumber.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+# Updates the application manifest to include the latest version number as specified by
+# the latest git tag and commit hash.
+cat "../ApplicationPackageRoot/ApplicationManifest.xml.template" | sed -i 's/{VERSION}/'"${git describe --tags}"'/g' > "../ApplicationPackageRoot/ApplicationManifest.xml"
